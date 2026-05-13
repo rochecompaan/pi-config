7a5ef83e335ee43aeed687cf22e7388114a11ca7
Roché Compaan <roche@upfrontsoftware.co.za>
2026-05-13 15:54:37 +0200

    feat(packages): extract pi dependency derivations

---
+++ modules/packages/notion-cli.nix
@@ -0,0 +1,39 @@
+args:
+if args ? options then
+  { }
+else
+  let
+    pkgs = args.pkgs;
+  in
+  pkgs.buildGoModule rec {
+    pname = "notion-cli";
+    version = "0.7.0";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "4ier";
+      repo = "notion-cli";
+      rev = "v${version}";
+      hash = "sha256-Wy3Xi40dsmk0igxsGiX7fqvgMVnuIcdNkOefUBAgy/I=";
+    };
+
+    vendorHash = "sha256-l+js7rA49aDVu6sHcuNDSv8R8E/Fi1J7yE17uaKHhjQ=";
+
+    ldflags = [
+      "-s"
+      "-w"
+      "-X github.com/4ier/notion-cli/cmd.Version=${version}"
+    ];
+
+    postInstall = ''
+      if [ -e "$out/bin/notion-cli" ]; then
+        mv "$out/bin/notion-cli" "$out/bin/notion"
+      fi
+    '';
+
+    meta = {
+      description = "Full-featured CLI for Notion";
+      homepage = "https://github.com/4ier/notion-cli";
+      license = pkgs.lib.licenses.mit;
+      mainProgram = "notion";
+    };
+  }
